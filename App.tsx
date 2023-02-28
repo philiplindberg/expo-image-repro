1b6970567355c0ca5a45c0be6236c51a2409a295
Philip Lindberg <philip.c.lindberg@gmail.com>
2023-02-28 11:06:59 +0000

    Add svgs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image
+        source='https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg'
+        contentFit='contain'
+        style={{ width: 150, height: 150 }}
+      />
+      <View style={{ height: 30 }} />
+      <Image
+        source={require('./assets/react-native-logo.svg')}
+        contentFit='contain'
+        style={{ width: 150, height: 150 }}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +28,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
